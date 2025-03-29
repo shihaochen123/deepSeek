dcc1b40f36f652ec203580ec383fa19946094f42
shc <1290285664@qq.com>
2025-03-30 00:43:07 +0800

    feat: 前端接入deepseek

---
+++ src/app/chat/[chat_id]/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useChat } from '@ai-sdk/react';
+
+export default function Page () {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({});
+
+  return (
+    <>
+      {messages.map(message => (
+        <div key={message.id}>
+          {message.role === 'user' ? 'User: ' : 'AI: '}
+          {message.content}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input name="prompt" value={input} onChange={handleInputChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
